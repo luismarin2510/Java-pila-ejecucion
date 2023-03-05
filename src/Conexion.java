45e0ff3f32b7db5984556f443ac9690b9128f7ba
Luismarin2510 <opraedu@gmail.com>
2023-03-05 10:09:37 -0500

    conexion y errores

---
+++ src/Conexion.java
@@ -0,0 +1,17 @@
+
+public class Conexion {
+
+	public Conexion() {
+		System.out.println("abriendo conexion");
+	}
+
+	public void leerdatos() {
+		System.out.println("Recibiendo datos");
+
+	}
+
+	public void Cerrar() {
+		System.out.println("Cerrando conexion ");
+	}
+
+}
