7a4c23eda501a64125d123b0df246f87d305981c
Luismarin2510 <opraedu@gmail.com>
2023-03-05 10:51:30 -0500

    uso de condigo entrelasado ... no recomendable

--- src/Testconexion.java
+++ src/Testconexion.java
@@ -3,14 +3,18 @@ public class Testconexion {
 
 	public static void main(String[] args) {
 
-		Conexion con = new Conexion();
+		/*Conexion con = new Conexion();
+		
+		 * try { con.leerdatos(); } catch (IllegalStateException ex) {
+		 * System.out.println("Recibiendo excepcion"); ex.printStackTrace(); } finally {
+		 * /// ejecuta el comando interno sin importar el resultado con.Cerrar(); }
+		 */
+		Conexion con = null;
 		try {
+			con = new Conexion();
 			con.leerdatos();
-
-		} catch (IllegalStateException ex) {
-			System.out.println("Recibiendo excepcion");
-			ex.printStackTrace();
-		} finally { /// ejecuta el comando interno sin importar el resultado
+		} finally {
+			System.out.println("ejecutanto finaly");
 			con.Cerrar();
 		}
 
